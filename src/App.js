b3e519f0528324295f6edc4b596701c1f695161e
nygupta <nygupta21@gmail.com>
2021-06-21 12:40:49 +0530

    board and square components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Board from "./components/Board";
 
 const App = () => {
   return (
-    <>
+    <div>
       <h1>TicTicToe App!</h1>
-    </>
+      <Board />
+    </div>
   );
 }
 
