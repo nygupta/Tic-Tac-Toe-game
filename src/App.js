d771b29883a9fd23c65c7e3a742d9752a1efeb7d
nygupta <nygupta21@gmail.com>
2021-06-21 23:14:11 +0530

    calculate winner and stop the game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import Board from "./components/Board";
+import { calculateWinner } from "./helpers";
 import "./style/root.scss";
 
 const App = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const[isXNext, setIsXNext] = useState(false);
+
+  const winner = calculateWinner(board);
+  const message = winner ? `Winner is ${winner}` : `Next Player is ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] || winner) return;
+
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position)
+          return isXNext ? 'X' : 'O';
+            return square;
+          });
+      });
+        
+    setIsXNext(prev => !prev);
+  };
+
+
   return (
     <div className="app">
       <h1>TicTicToe App!</h1>
-      <Board />
+      <h3>{ message }</h3>
+      <Board board={board} handleSquareClick={handleSquareClick}/>
     </div>
   );
 }
